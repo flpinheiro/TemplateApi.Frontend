1996794fdabf7412f50240709f7a83f7dade79d5
Matheus Phillipo Silverio Silva <matheusphillipo@gmail.com>
2022-09-05 17:45:43 -0300

    Estrutura da aplicação

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import {ThemeProvider} from 'styled-components'
+
+function App() {
+
+  return (
+    <>
+      <ThemeProvider theme={undefined}>
+
+
+      </ThemeProvider>
+    </>
+  )
+}
+
+export default App
